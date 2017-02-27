b99319da726d10a5fe3468f03fe824f9a69f43a6
vehonsaimia <vesa.hongisto@student.saimia.fi>
2017-02-27 12:08:23 +0200

    String Exercise, tasks 1-3

---
+++ Exercise_strings/Exercise_strings/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Task 1 counts the number of the characters in the given string. Time used 15 minutes.
+            
+            Console.WriteLine("Please give a string  ");
+            string checkme = Console.ReadLine();
+
+            Console.WriteLine("String length was " + checkme.Length + " characters");
+
+            Console.WriteLine("Press any key to finish ");
+            Console.ReadLine();
+        }
+    }
+}
