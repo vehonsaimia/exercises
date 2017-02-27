58007f8a19a2bde146bdbac72f060c4a54e23cef
vehonsaimia <vesa.hongisto@student.saimia.fi>
2017-02-27 10:57:10 +0200

    String tasks

---
+++ Strings_task2/Strings_task2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strings_task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
